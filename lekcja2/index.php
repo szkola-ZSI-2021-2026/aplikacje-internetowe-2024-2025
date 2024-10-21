66e67ace120f8a3c9361f4e8912a057facc69cb5
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2024-10-21 12:35:49 +0200

    fd

--- lekcja2/index.php
+++ lekcja2/index.php
@@ -8,7 +8,20 @@
 <body>
 
     <?php
-        echo "test";
+        $a = 1;
+
+        $b = $c = 2;
+
+        $a += $b + $c;
+
+        echo $a--;
+
+        $dzielenie = $a % 2;
+        echo $dzielenie;
+
+        $b *= 3;
+
+        echo "Wartość zmiennej \$a wynosi ".$a.", zmiennej \$b wynosi ".$b.", a zmiennej \$c ".$c;
     ?>
 
 </body>
