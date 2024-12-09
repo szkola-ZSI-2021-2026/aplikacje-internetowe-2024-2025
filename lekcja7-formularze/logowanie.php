4c30b7340c4dbff6ff37a04396f8743c95298df0
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2024-12-09 12:15:24 +0100

    09.12.2024

--- lekcja7-formularze/logowanie.php
+++ lekcja7-formularze/logowanie.php
@@ -6,10 +6,13 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Dane logowania</h1>
     <?php
-    
-    
-    
+
+        echo "Login: ".$_POST['login']."<br>";
+        echo "Password: ".$_POST['password']."<br>";
+        echo "Zapametaj: ".$_POST['remember']."<br>";
+
     ?>
 </body>
 </html>
