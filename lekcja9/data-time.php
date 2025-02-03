b9b82a181d914d72265cf2d4441c28775bce9212
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-02-03 14:38:02 +0100

    dgfdgfd

--- lekcja9/data-time.php
+++ lekcja9/data-time.php
@@ -61,11 +61,69 @@
         echo "Rozpoczęcie roku szkolnego odbyło się ".date("d.m.Y H:i:s", $rozpoczecieRokuSzkolnego)."<br>";
     
     
-        // strtotime() - przekształca anngielskie zwroty na czas
+        // strtotime() - przekształca anngielskie zwroty na czas (sekundy)
         echo "<br>";
         echo "<hr>";
-        echo strtotime("now")."<br>";
-    
+        echo "Now ".strtotime("now")."<br>";
+        echo "+1 day ".strtotime("+1 day")."<br>";
+        echo "+5 days ".strtotime("+5 days")."<br>";
+        echo "+1 week ".strtotime("+1 week")."<br>";
+        echo "+8 hours ".strtotime("+8 hours")."<br>";
+        echo "6 stycznia 2025 ".strtotime("6 January 2025")." | ".date("d.m.Y H:i:s", strtotime("6 January 2025"))."<br>";
+        echo "Nastepna środa ".strtotime("next Wednesday")."<br>";
+        echo "Seszły poniedziałek ".date("jS F l",strtotime("last Monday"))."<br>";
+        echo "Zeszły poniedziałek time() ".date("jS F l",time()-(60*60*24*7)),"<br>";
+    ?>
+
+    <hr>
+    <h1>Ćwiczenia</h1>
+    <h3>Zad 1</h3>
+    <?php
+        $dzienUrodzenia = mktime(0,0,1,9,24,2006);
+        echo date("jS F l",$dzienUrodzenia)."<br>";
+        print_r(getdate($dzienUrodzenia));
+        echo "<br>";
+        echo "Urodziłem się ", getdate($dzienUrodzenia)["weekday"];
+    ?>
+    <h3>Zad 2</h3>
+    <h4>sprawdz ile czasu zostało do walentynek</h4>
+    <?php
+        $walentynki = "2025-02-14";
+        $dzienWalentynek = date("l",strtotime($walentynki));
+        $polskieDni = [
+            "Monday" => "Poniedziałek",
+            "Tuesday" => "Wtorek", 
+            "Wednesday" => "Sroda", 
+            "Thursday" => "Czwartek", 
+            "Friday" => "Piątek", 
+            "Saturday" => "Sobota", 
+            "Sunday" => "Niedziela" 
+        ];
+        echo "Walentynki są w ".$polskieDni[$dzienWalentynek]."<br>";
+        $ileCzsuWSek = (strtotime($walentynki) - strtotime("now"))/(60*60*24);
+        echo "<b style='color:red'>Walentynki</b> są za ",ceil($ileCzsuWSek)," dni.<br>";
+    ?>
+
+    <h3>Zad 3</h3>
+    <h4>Wyświetl baner zależny od pory dnia</h4>
+    <?php
+
+        function banner() {
+            $godzina = date("G");
+            $godzina = 12;
+            
+            if($godzina>22 || $godzina<6){
+                return 'noc.jpg';
+            } elseif ($godzina < 8) {
+                return 'wschod.jpg';
+            } elseif ($godzina < 19) {
+                return 'dzien.jpg';
+            } else {
+                return 'zachod.jpg';
+            }
+        }
+
+        echo "<img src=img/".banner()." height=400px width=800px>";
     ?>
 </body>
 </html>
