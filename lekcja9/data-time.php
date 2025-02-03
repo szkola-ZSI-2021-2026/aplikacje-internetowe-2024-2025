33b47314ef9a0d1282c8186242220e7fc60d8e47
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-02-03 13:05:45 +0100

    fgh

--- lekcja9/data-time.php
+++ lekcja9/data-time.php
@@ -9,6 +9,7 @@
     <?php
         // time() - zwraca ilośc sekund od znacznika czasu 00:00 01.01.1970r.
         echo "Liczba sekund: ".time()."<br>";
+        // getdate() - zwraca tablicę asocjacyjną z informacjami o dacie i czasie
         $date = getdate();
         
         echo "Weekday: ".$date["weekday"]."<br>";
@@ -21,6 +22,50 @@
         }
         echo "<br>";
 
+        $dzien = $date["mday"];
+        $miesiac = $date["mon"];
+        $rok = $date["year"];
+
+        if($dzien < 10) {
+            $dzien = "0".$dzien;
+        }
+        if($miesiac < 10) {
+            $miesiac = "0".$miesiac;
+        }
+
+        echo "Dzisiejsza data: $dzien.$miesiac.$rok <br>";
+        echo "Aktualna godina ".$date["hours"].":".$date["minutes"].":".$date["seconds"]."<br>";
+
+
+        // date() - formatuje datę i godzinę dzięki znacznikom zwracając string
+        echo "<br>";
+        echo "<hr>";
+        echo date("d.m.Y");
+        echo "<br>";
+        echo date("H.i.s");
+        echo "<br>";
+        echo date("jS M Y");
+        echo "<br>";
+        echo date("D F y");
+        echo "<br>";
+        echo date("g a");
+    
+    
+        // mktime() - ustawia wybraną datę i godzinę zwracając liczbe sekund
+        echo "<br>";
+        echo "<hr>";
+        $rozpoczecieRokuSzkolnego = mktime(9,0,0,9,2,2024);
+        echo $rozpoczecieRokuSzkolnego."<br>";
+        print_r(getdate($rozpoczecieRokuSzkolnego));
+        echo "<br>";
+        echo "Rozpoczęcie roku szkolnego odbyło się ".date("d.m.Y H:i:s", $rozpoczecieRokuSzkolnego)."<br>";
+    
+    
+        // strtotime() - przekształca anngielskie zwroty na czas
+        echo "<br>";
+        echo "<hr>";
+        echo strtotime("now")."<br>";
+    
     ?>
 </body>
 </html>
