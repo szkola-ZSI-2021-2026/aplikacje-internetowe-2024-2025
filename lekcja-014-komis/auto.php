ca30cedc6f1fcf5b0d866e6bd2dcf4947fff7340
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-03-10 13:52:05 +0100

    13.51

--- lekcja-014-komis/auto.php
+++ lekcja-014-komis/auto.php
@@ -14,23 +14,45 @@
         <h2>Wykaz samochodów</h2>
         <ul>
             <?php
-            // skrypt 1
-
+                // skrypt 1
+                $idConnection = mysqli_connect(
+                    "localhost", "root", "", "komis"
+                );
+                $q1 = "SELECT id, marka, model from samochody;";
+                $res1 = mysqli_query($idConnection, $q1);
+                // print_r($res1);
+                while($row=mysqli_fetch_row($res1)){
+                    echo "<li> $row[0] $row[1] $row[2] </li>";
+                }
+                print_r($row);
             ?>
         </ul>
         <h2>Zamówienia</h2>
         <ul>
             <?php
-            // skrypt 2
-
+                // skrypt 2
+                $q2 = "SELECT zamowienia.Samochody_id, zamowienia.Klient FROM zamowienia;";
+                $res2 = mysqli_query($idConnection, $q2);
+                while($row=mysqli_fetch_row($res2)){
+                    echo "<li> $row[0] $row[1] </li>";
+                }
             ?>
         </ul>
     </section> 
     <section id="right">
         <h2>Pełne dane: Fiat</h2>
         <?php
-        // skrypt 3
-        
+            // skrypt 3
+            $q3 = "SELECT * from samochody where samochody.marka='fiat';";
+            $res3 = mysqli_query($idConnection, $q3);
+            while($row=mysqli_fetch_row($res3)){
+                echo "$row[0] / $row[1] / $row[2] / $row[3] / $row[4] / $row[5] / <br>"; 
+            }
+
+            // $arr=mysqli_fetch_array($res3)
+            // print_r($arr);
+
+            mysqli_close($idConnection);
         ?>
     </section>
     <footer>
