a94caeb59e193433888fff589e37b527787ea035
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-03-24 13:08:39 +0100

    13.08

---
+++ lekcja-018-rybki_drapierzne-2019/wedkuj.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wedkujemy</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+
+    <main>
+        <div id="left">
+            <h2>Ryby drapieżne wśród naszych wód</h2>
+
+            <ul>
+                <?php
+                
+                    $conn = mysqli_connect("localhost", "root", "", "wedkowanie2");
+
+                    $query = "SELECT nazwa, wystepowanie from ryby where styl_zycia = 1;";
+
+                    $result = mysqli_query($conn, $query);
+
+                    while($row = mysqli_fetch_row($result)) {
+                        echo "<li> $row[0]  $row[1] </li>";
+                    }
+
+                    mysqli_close($conn);
+                ?>
+            </ul>
+        </div>
+
+        <div id="right">
+            <img src="pliki1/ryba1.jpg" alt="Sum">
+
+            <a href="kwerendy.txt">Pobierz kwerendy</a>
+        </div>
+    </main>
+    
+    <footer>
+        <p>Stronę wykonał: PESEL</p>
+    </footer>
+</body>
+</html>
