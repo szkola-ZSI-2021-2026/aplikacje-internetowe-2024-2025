44744c576cd8faf179c6c6e18fb726b98b433290
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-03-24 14:28:19 +0100

    14.30

--- lekcja-019-pogotowie_ratunkowe-2019/zgloszenie.php
+++ lekcja-019-pogotowie_ratunkowe-2019/zgloszenie.php
@@ -5,7 +5,12 @@
     $dyspozytor = $_POST['dyspozytor'];
     $adres = $_POST['adres'];
 
-    $query = "INSERT INTO zgloszenia VALUES (null,$zespol,$dyspozytor,'$adres',0,CURRENT_TIME);";
+    $time = time();
+    //echo $time."<br>";
+    $time = date("H:i:s", $time);
+    //echo $time;
+
+    $query = "INSERT INTO zgloszenia VALUES (null,$zespol,$dyspozytor,'$adres',0,'$time');";
 
     mysqli_query($conn, $query);
 
