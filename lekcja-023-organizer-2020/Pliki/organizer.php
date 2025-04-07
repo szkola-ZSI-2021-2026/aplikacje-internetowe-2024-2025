3701345079a990c11c07f6a35dde06853e1a17e8
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-04-07 14:22:11 +0200

    14.20-7.04.2025

--- lekcja-023-organizer-2020/Pliki/organizer.php
+++ lekcja-023-organizer-2020/Pliki/organizer.php
@@ -17,18 +17,20 @@
             <form action="organizer.php" method="post">
                 Wpis wydarzenia: 
                 <input type="text" name="wpis">
-                <input type="submit" value="ZAPISZZ">
+                <input type="submit" value="ZAPISZ">
             </form>
 
-            // uaktualnienie bazy danych
+            <!-- uaktualnienie bazy danych -->
             <?php
                 $connection = mysqli_connect("localhost", "root", "", "egzamin6_2");
 
-                $wpis = $_POST['wpis'];
+                if(!empty($_POST['wpis'])) {
+                    $wpis = $_POST['wpis'];
 
-                $query1 = "UPDATE zadania SET zadania.wpis = '$wpis' WHERE zadania.dataZadania = '2020-08-27';";
+                    $query1 = "UPDATE zadania SET zadania.wpis = '$wpis' WHERE zadania.dataZadania = '2020-08-27';";
 
-                mysqli_query($connection, $query1);
+                    mysqli_query($connection, $query1);
+                }
             ?>
         </div>
         <div class="right">
@@ -37,21 +39,41 @@
     </header>
 
     <main>
-        // skrypt 1
+        <!-- skrypt 1 -->
         <?php
             $query2 = "SELECT zadania.dataZadania, zadania.miesiac, zadania.wpis FROM zadania WHERE zadania.miesiac = 'Sierpien';";
         
-            function printBlock($date, $wpis) {
-                echo "<section>";
-                echo "<h6> </h6>";
-                echo "<p> </p>";
+            function printBlock($date, $miesiac, $wpis) {
+                echo "<section class='oneDay'>";
+                echo "<h6>$date, $miesiac</h6>";
+                echo "<p>$wpis</p>";
                 echo "</section>";
+            }
+
+            $result1 = mysqli_query($connection, $query2);
+
+            while($row = mysqli_fetch_row($result1)) {
+                printBlock($row[0], $row[1], $row[2]);
             }
         ?>
     </main>
 
     <footer>
-        // skrypt 2
+       <!-- skrypt 2 -->
+        <?php
+            $query3 = "SELECT zadania.miesiac, zadania.rok FROM zadania WHERE zadania.dataZadania = '2020-08-01';";
+
+            $result2 = mysqli_query($connection, $query3);
+
+            while ($row = mysqli_fetch_row($result2)) {
+                echo "<h1>";
+                echo "miesiąc: $row[0], rok: $row[1]";
+                echo "</h1>";
+            }
+
+            mysqli_close($connection);
+        ?>
+
         <p>
             Stronę wykonał PESEL
         </p>
